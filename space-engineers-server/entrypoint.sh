eca0cdf5fe8082d3acca8e6ed94cf46e56cd1131
Cameron Boulton <cameronboulton@gmail.com>
2016-02-03 16:23:39 -0800

    Added steam group to entrypoint script's chown.

--- space-engineers-server/entrypoint.sh
+++ space-engineers-server/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = 'space-engineers-server' ]; then
-	[ $(whoami) = root ] && chown -R steam . && su -c "$0 $*" steam
+	[ $(whoami) = root ] && chown -R steam:steam . && su -c "$0 $*" steam
 	/home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /mnt/steam/space-engineers-server/ +app_update 298740 +quit
 	wine DedicatedServer/SpaceEngineersDedicated.exe -console
 fi
